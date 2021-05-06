c8392ce5cf0569dd4ca4d059a5d52e9626125763
canyetismis <can.yetismis@gmail.com>
2021-05-06 12:58:11 +0300

    Added a dummy login page and home page

--- app/src/main/java/com/canyetismis/football_app/MainActivity.java
+++ app/src/main/java/com/canyetismis/football_app/MainActivity.java
@@ -2,13 +2,26 @@ package com.canyetismis.football_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.canyetismis.football_app.view.home.HomeActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login);
+    }
+
+    public void onLoginClicked(View view){
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/canyetismis/football_app/view/home/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.canyetismis.football_app.view.home;
+import com.canyetismis.football_app.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
