866182604199dd08969cb9e35c727529daa37137
canyetismis <can.yetismis@gmail.com>
2021-05-12 12:16:56 +0300

    Fixes on code structure

--- app/src/main/java/com/canyetismis/football_app/model/Team.java
+++ app/src/main/java/com/canyetismis/football_app/model/Team.java
@@ -30,22 +30,3 @@ public class Team {
         this.teamName = teamName;
     }
 }
-/*
-public class Team {
-    private String teamName;
-
-    public Team(String teamName){
-        this.teamName = teamName;
-    }
-
-    public String getTeamName(){
-        return teamName;
-    }
-
-    public void setTeamName(String teamName){
-        this.teamName = teamName;
-    }
-}
-
-
- */
